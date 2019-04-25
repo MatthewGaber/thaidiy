a3788460074a9153d544c9836aa8ddf7d1d4ee02
MatthewGaber <matthew.gaber@teknojelly.com>
2019-04-25 14:02:05 +0700

    added environ vari debug and secret key

--- thaidiy/thaidiy/settings.py
+++ thaidiy/thaidiy/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(m17j(=9!0$)l$!i88-qtdst-k1d!w5d0!d71wq@5^=-%3c(er'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_VALUE")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [thaidiyninja.herokuapp.com]
 
 
 # Application definition
