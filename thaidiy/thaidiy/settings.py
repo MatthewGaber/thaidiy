a8a4ae85c7373c3f6c44e4ae355fd00f20cbcc91
MatthewGaber <matthew.gaber@teknojelly.com>
2019-04-26 15:14:10 +0700

    added not image file for ckeditro

--- thaidiy/thaidiy/settings.py
+++ thaidiy/thaidiy/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+CKEDITOR_ALLOW_NONIMAGE_FILES = False
 LOGIN_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
