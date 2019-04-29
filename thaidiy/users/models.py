d1ddeda0643ac869e0113eaa9b9efddc8e4843ad
MatthewGaber <matthew.gaber@teknojelly.com>
2019-04-29 10:26:14 +0700

    comment to force

--- thaidiy/users/models.py
+++ thaidiy/users/models.py
@@ -29,7 +29,7 @@ class Profile(models.Model):
 
         img_read.close()
 
-# old resize code not working
+# old resize code not working outdated
     """ def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
